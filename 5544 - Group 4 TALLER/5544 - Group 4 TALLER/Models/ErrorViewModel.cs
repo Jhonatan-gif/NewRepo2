71d9b75b11b15427e8f4acaea2a51ac52b8d524d
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2025-04-30 19:31:35 -0500

    Agregar archivos de proyecto.

---
+++ 5544 - Group 4 TALLER/5544 - Group 4 TALLER/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _5544___Group_4_TALLER.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
